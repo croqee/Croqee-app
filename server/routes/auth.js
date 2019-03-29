004b1a335f1591781c0f4455899038e7bc4b62bb
arashabedin <arash.abedin@gmail.com>
2019-03-29 23:01:58 +0100

    fix null exception

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -129,7 +129,6 @@ router.post('/signup', (req, res, next) => {
 router.post('/login',(req, res, next) => {
 
 req.body = croqeeBodyParser(req.body)
-console.log(req.body)
 const validationResult = validateLoginForm(req.body);
   if (!validationResult.success) {
     return res.status(400).json({
