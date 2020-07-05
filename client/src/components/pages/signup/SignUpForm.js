9c728dce1cd046533a19e45257cab199bc39100b
Sonia <soniamianji1@gmail.com>
2020-07-05 14:57:27 +0200

    restyling signup page/form

--- client/src/components/pages/signup/SignUpForm.js
+++ client/src/components/pages/signup/SignUpForm.js
@@ -1,64 +1,74 @@
-import React, { PropTypes } from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { theme } from "../../child/MuiTheme";
 
 const SignUpForm = ({ onSubmit, onChange, errors, user }) => (
-	<div className="container">
-		<form action="/" onSubmit={onSubmit} className="auth_form">
-			<h2 className="card-heading">Sign Up</h2>
+  <MuiThemeProvider theme={theme}>
+    <div className="container">
+      <form action="/" onSubmit={onSubmit} className="auth_form" noValidate>
+        <h2 className="card-heading">Sign Up</h2>
 
-			{errors.summary && <p className="error-message">{errors.summary}</p>}
+        {errors.summary && <p className="error-message">{errors.summary}</p>}
 
-			<div className="field-line">
-				<label for="email">Name</label>
-				<div className="auth_form_error">{errors.name}</div>
-				<input
-					floatingLabelText="Name"
-					className="auth_input"
-					id="name"
-					name="name"
-					onChange={onChange}
-					value={user.name}
-				/>
-			</div>
+        <div className="field-line">
+          <TextField
+            required
+            label="Username"
+            id="name"
+            name="name"
+            onChange={onChange}
+            value={user.name}
+            placeholder="Your Username"
+            error={Boolean(
+              errors.name !== undefined || errors.summary !== undefined
+            )}
+            helperText={
+              errors.name === undefined ? errors.summary : errors.name
+            }
+          />
+        </div>
 
-			<div className="field-line">
-				<label for="email">Email</label>
-				<div className="auth_form_error">{errors.email}</div>
-				<input
-					floatingLabelText="Email"
-					className="auth_input"
-					id="email"
-					name="email"
-					onChange={onChange}
-					value={user.email}
-				/>
-			</div>
+        <TextField
+          required
+          label="Email"
+          id="email"
+          name="email"
+          onChange={onChange}
+          value={user.email}
+          placeholder="Your Email address"
+          error={Boolean(
+            errors.email !== undefined || errors.summary !== undefined
+          )}
+          helperText={
+            errors.email === undefined ? errors.summary : errors.email
+          }
+        />
 
-			<div className="field-line">
-				<label for="password">Password</label>
-				<div className="auth_form_error">{errors.password}</div>
-				<input
-					floatingLabelText="Password"
-					className="auth_input"
-					id="password"
-					type="password"
-					name="password"
-					onChange={onChange}
-					value={user.password}
-				/>
-			</div>
+        <TextField
+          required
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onChange={onChange}
+          value={user.password}
+          placeholder="Your Password "
+          error={Boolean(
+            errors.password !== undefined || errors.summary !== undefined
+          )}
+          helperText={
+            errors.password === undefined ? errors.summary : errors.password
+          }
+        />
 
-			<div className="button-line">
-				<button type="submit" className="auth_button">
-					Create Account
-				</button>
-			</div>
-
-			<div>
-				Already have an account? <Link to={'/signin'}>Log in</Link>
-			</div>
-		</form>
-	</div>
+        <Button variant="outlined" type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  </MuiThemeProvider>
 );
 
 // SignUpForm.propTypes = {
--- client/src/components/pages/signup/SignUpPage.js
+++ client/src/components/pages/signup/SignUpPage.js
@@ -1,102 +1,164 @@
-import React, { PropTypes } from 'react';
-import SignUpForm from './SignUpForm.js';
-import { Redirect } from 'react-router-dom';
-import config from '../../../modules/config';
-import axios from 'axios';
+import React, { PropTypes } from "react";
+import SignUpForm from "./SignUpForm.js";
+import Auth from "../../../modules/Auth";
+import { Link } from "react-router-dom";
+import config from "../../../modules/config";
+import axios from "axios";
+import { Card, Button, Typography } from "@material-ui/core";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { theme } from "../../child/MuiTheme";
+import { GoogleLogin } from "react-google-login";
+import { googleApiKey } from "../../../clientglobalvariables";
+import {
+  setUser,
+  getUser,
+  setPageToNavigateAfterLogin
+} from "../../../js/actions";
+import { connect } from "react-redux";
 
 class SignUpPage extends React.Component {
-	/**
+  /**
    * Class constructor.
    */
-	constructor(props, context) {
-		super(props, context);
+  constructor(props, context) {
+    super(props, context);
 
-		// set the initial component state
-		this.state = {
-			errors: {},
-			user: {
-				email: '',
-				name: '',
-				password: ''
-			}
-		};
+    // set the initial component state
+    this.state = {
+      errors: {},
+      user: {
+        email: "",
+        name: "",
+        password: ""
+      }
+    };
 
-		this.processForm = this.processForm.bind(this);
-		this.changeUser = this.changeUser.bind(this);
-	}
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
 
-	/**
+  /**
    * Process the form.
    *
    * @param {object} event - the JavaScript event object
    */
-	processForm(event) {
-		// prevent default action. in this case, action is the form submission event
-		event.preventDefault();
+  processForm(event) {
+    // prevent default action. in this case, action is the form submission event
+    event.preventDefault();
 
-		// create a string for an HTTP body message
-		const name = encodeURIComponent(this.state.user.name);
-		const email = encodeURIComponent(this.state.user.email);
-		const password = encodeURIComponent(this.state.user.password);
+    // create a string for an HTTP body message
+    const name = encodeURIComponent(this.state.user.name);
+    const email = encodeURIComponent(this.state.user.email);
+    const password = encodeURIComponent(this.state.user.password);
 
-		const body = {
-			name,
-			email,
-			password
-		};
+    const body = {
+      name,
+      email,
+      password
+    };
 
-		let UnAthorizedHeader = config.UnAthorizedHeader();
+    let UnAthorizedHeader = config.UnAthorizedHeader();
 
-		axios
-			.post('/auth/signup', body, UnAthorizedHeader)
-			.then((response) => {
-				this.setState({
-					errors: {}
-				});
-				localStorage.setItem('successMessage', response.data.message);
-				return this.props.history.push('/login');
-			})
-			.catch((error) => {
-				console.log(error.response);
-				let { errors } = error.response.data;
-				this.setState({
-					errors
-				});
-			});
-	}
+    axios
+      .post("/auth/signup", body, UnAthorizedHeader)
+      .then(response => {
+        this.setState({
+          errors: {}
+        });
+        localStorage.setItem("successMessage", response.data.message);
+        return this.props.history.push("/login");
+      })
+      .catch(error => {
+        let { errors } = error.response.data;
+        this.setState({
+          errors
+        });
+      });
+  }
 
-	/**
+  /**
    * Change the user object.
    *
    * @param {object} event - the JavaScript event object
    */
-	changeUser(event) {
-		const field = event.target.name;
-		const user = this.state.user;
-		user[field] = event.target.value;
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
 
-		this.setState({
-			user
-		});
-	}
+    this.setState({
+      user
+    });
+  }
 
-	/**
+  responseGoogle = response => {
+    axios
+      .post("auth/googleauth", { googleCode: response.code })
+      .then(res => {
+        const { token, user } = res.data;
+        this.props.setUser(user);
+        this.setState({
+          errors: {}
+        });
+        Auth.authenticateUser(token);
+        this.props.getUser();
+        this.props.history.push(this.props.pageToNavigateAfterLogin);
+        return this.props.setPageToNavigateAfterLogin("/");
+      })
+      .catch(err => console.log(err));
+  };
+
+  /**
    * Render the component.
    */
-	render() {
-		return (
-			<SignUpForm
-				onSubmit={this.processForm}
-				onChange={this.changeUser}
-				errors={this.state.errors}
-				user={this.state.user}
-			/>
-		);
-	}
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Card variant="outlined" square dark>
+          <SignUpForm
+            onSubmit={this.processForm}
+            onChange={this.changeUser}
+            errors={this.state.errors}
+            user={this.state.user}
+          />
+          <GoogleLogin
+            clientId={googleApiKey}
+            render={renderProps => (
+              <Button
+                variant="contained"
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<i class="fab fa-google"></i>}
+              >
+                Continue With Google
+              </Button>
+            )}
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+            cookiePolicy={"single_host_origin"}
+            responseType="code"
+            accessType="offline"
+          />
+        </Card>
+        <Typography variant="caption" display="block" gutterBottom>
+          Already a member? <Link to={"/signin"}>Login</Link>.
+        </Typography>
+      </MuiThemeProvider>
+    );
+  }
 }
 
-// SignUpPage.contextTypes = {
-//   router: PropTypes.object.isRequired
-// };
+const mapStateToProps = state => {
+  const { pageToNavigateAfterLogin } = state;
+  return { pageToNavigateAfterLogin };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    getUser: () => dispatch(getUser()),
+    setUser: user => dispatch(setUser(user)),
+    setPageToNavigateAfterLogin: payload =>
+      dispatch(setPageToNavigateAfterLogin(payload))
+  };
+};
 
-export default SignUpPage;
+export default connect(mapStateToProps, mapDispatchToProps)(SignUpPage);
