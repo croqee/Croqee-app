80d1548eb81caccb50303ac88a21c98de995c569
Arash Abedin <arash.abedin@ffwagency.com>
2020-07-27 19:33:30 +0200

    compile ts

--- server/dist/routes/images.js
+++ server/dist/routes/images.js
@@ -44,7 +44,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
     storage: storage,
     limits: {
-        fileSize: 250 * 250
+        fileSize: 1 * 1024 * 1024
     },
     fileFilter: fileFilter
 });
