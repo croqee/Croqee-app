92828802896b4fc4b72ee7dd10824e7d07371d65
Sonia <soniamianji1@gmail.com>
2020-07-30 18:58:20 +0200

    edit footer styles and responsiveness

--- client/src/components/child/footer/Footer.js
+++ client/src/components/child/footer/Footer.js
@@ -3,6 +3,39 @@ import Links from "../navbar/Links";
 import Social from "../navbar/Social";
 import ContactInfo from "../navbar/ContactInfo";
 import { TextField } from "@material-ui/core";
+//import { theme } from "../MuiTheme";
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import ActionBtnNav from "../navbar/ActionBtnNav";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  overrides: {
+    MuiInput: {
+      underline: {
+        "&:hover:not(.Mui-disabled):before": {
+          borderBottom: "none"
+        },
+        "&:hover:not($disabled):not($error):before": {
+          borderBottom: "none"
+        },
+        "&:before": {
+          borderBottom: "none"
+        },
+        "&:after": {
+          borderBottom: "1px solid rgba(255, 255, 255, 0.82)"
+        },
+        color: "#b8b8b8",
+        width: "265px"
+      }
+    },
+    MuiInputBase: {
+      input: {
+        padding: "1rem 0",
+        color: "#b8b8b8"
+      }
+    }
+  }
+});
 
 export default function Footer() {
   return (
@@ -15,14 +48,17 @@ export default function Footer() {
           <h3>Sign up and join the Croqee Community!</h3>
           <div className="footer-top--newsLetter-form">
             <form>
-              <input
-                className="footer-top--newsLetter-input"
-                type="text"
-                placeholder="Enter e-mail address"
-              />
-              <button type="submit" className="footer-top--newsLetter-btn">
-                <span className="footer-top--newsLetter-btn-text">Sign Up</span>
-              </button>
+              <MuiThemeProvider theme={theme}>
+                <TextField
+                  type="text"
+                  //className="profile__userInfo__container__form__input"
+                  error="false"
+                  placeholder="Enter e-mail address"
+                />
+              </MuiThemeProvider>
+              <div className="footer-top--newsLetter-form-btn">
+                <ActionBtnNav btnText={"Sign Up"} />
+              </div>
             </form>
           </div>
           <small>
