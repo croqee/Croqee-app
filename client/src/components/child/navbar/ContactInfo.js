c40d83328f109aebd14190b25d2c584384f91f8d
arashffw <52411567+arashffw@users.noreply.github.com>
2020-08-21 10:24:28 +0200

    Change contact info

--- client/src/components/child/navbar/ContactInfo.js
+++ client/src/components/child/navbar/ContactInfo.js
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 export default function ContactInfo() {
   return (
     <Fragment>
-      <p>info@crooqi.com</p>
-      <p>+45 888 665 554</p>
+      <p>info@croqee.com</p>
+      <p>+45 60 52 60 31</p>
     </Fragment>
   );
 }
