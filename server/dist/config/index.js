6241515951a20fe6fbc9bf5ec1f5b32485d6e6e9
Alireza Asgharizadeh <alireza4050@yahoo.com>
2021-02-02 00:05:26 +0330

    deleted some unnecessary files

--- server/dist/config/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var config;
-(function (config) {
-    config.dbUri = "mongodb+srv://croqee:yamaha3000@cluster0-9lzas.mongodb.net/test?retryWrites=true&w=majority";
-    config.jwtSecret = "a secret phrase!!";
-})(config || (config = {}));
-module.exports = config;
-//# sourceMappingURL=index.js.map
--- server/dist/db/models/score.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const ScoreSchema = new mongoose.Schema({
-    userId: String,
-    modelId: String,
-    score: Number,
-    date: Date
-});
-module.exports = mongoose.model('Score', ScoreSchema);
-//# sourceMappingURL=score.js.map
