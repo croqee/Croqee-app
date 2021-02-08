c30e39bd670c94b55241bdcd12dd0ac288e81648
arash abedin <arash.abedin@ffwagency.com>
2021-02-08 08:05:00 +0100

    modify loader style

--- client/src/components/child/loader/Loader.js
+++ client/src/components/child/loader/Loader.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class Loader extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			fadeIn: ''
+		}
+	}
+	componentDidMount() {
+		this.setState({
+			fadeIn: 'loader-canvas-blocker--fade-in'
+		});
+	}
 	render() {
+		const { fadeIn } = this.state;
 		return (
-			<span className="loader"><span className="loader__inner"></span></span>
+			<React.Fragment>
+				<div className={"loader-canvas-blocker " + fadeIn}></div>
+				<span className="loader"><span className="loader__inner"></span></span>
+			</React.Fragment>
 		);
 	}
 }
