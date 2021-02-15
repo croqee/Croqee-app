140387d230ef2fe597558f86a707d5e6ddc661a7
arashffw <52411567+arashffw@users.noreply.github.com>
2021-02-15 18:11:04 +0100

    deploy fixes

--- server/dist/index.js
+++ server/dist/index.js
@@ -14,7 +14,7 @@ const app = express();
 const logger = require("morgan");
 const socketIO = require("socket.io");
 var ioClient = require("socket.io-client");
-const socketClient = ioClient.connect("http://localhost:9699", {
+const socketClient = ioClient.connect("http://server_python:9699", {
     reconnect: true
 });
 require("./db/models").connect(config_1.default.dbUri);
