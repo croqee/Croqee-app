4ac4b5321e12737a97bd00c11c97fc3f6099b5ff
Alireza Asgharizadeh <alireza4050@yahoo.com>
2021-03-28 23:32:51 +0430

    dto

---
+++ server/src/controllers/drawing-competition/dtos.ts
@@ -0,0 +1,20 @@
+import { IsEmail, MinLength } from 'class-validator';
+
+export class SignUpDto {
+  @IsEmail()
+  email: string;
+
+  @MinLength(8)
+  password: string;
+
+  @MinLength(1)
+  name: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @MinLength(8)
+  password: string;
+}
